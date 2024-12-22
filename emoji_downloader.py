613adb29082c02f3ed47de0a317a3d68a676a378
Panda <46421954+AnthonyCodeDev@users.noreply.github.com>
2024-12-22 22:41:28 +0100

    Update emoji_downloader.py

--- emoji_downloader.py
+++ emoji_downloader.py
@@ -1,57 +1,90 @@
-#!/bin/python3
-import requests, os, shutil 
+import os
+import shutil
+import requests
 
+class DiscordEmojiDownloader:
+    def __init__(self):
+        self.token = self.get_token()
+        self.current_dir = os.path.dirname(os.path.abspath(__file__))
 
-if os.path.isfile("./token.txt"):
-    token = open("token.txt").read().strip()
-else:
-    token = input("token : ")
-    open("token.txt", "w").write(token)
+    def get_token(self):
+        if os.path.isfile("./token.txt"):
+            return open("token.txt").read().strip()
+        else:
+            token = input("Enter your Discord token: ").strip()
+            with open("token.txt", "w") as file:
+                file.write(token)
+            return token
 
-def restrict(str_):
-    if os.name == "nt":
-        str_ = str_.translate({ord(i): None for i in '\/:*?"<>|'})
-    else:
-        str_ = str_.replace("/", "")
-    if len(str_) == 0:
-        str_ = "no name"
-    return str_
+    @staticmethod
+    def restrict(name):
+        if os.name == "nt":
+            name = name.translate({ord(i): None for i in '\\/:*?"<>|'})
+        else:
+            name = name.replace("/", "")
+        return name if name else "no_name"
+
+    def fetch_guilds(self):
+        response = requests.get(
+            "https://discord.com/api/v8/users/@me/guilds",
+            headers={"authorization": self.token}
+        )
+        if response.status_code != 200:
+            print(f"Failed to fetch guilds: {response.status_code} {response.text}")
+            exit(1)
+        return response.json()
+
+    def choose_guild(self, guilds):
+        for i, guild in enumerate(guilds, start=1):
+            print(f"{i} | {guild['name']}")
+
+        choice = input("Choose the guild by number: ")
+        if choice.isdigit() and 1 <= int(choice) <= len(guilds):
+            selected_guild = guilds[int(choice) - 1]
+            return selected_guild["id"], selected_guild["name"]
+        else:
+            print("Invalid selection")
+            exit(1)
+
+    def prepare_folder(self, folder_name):
+        folder_path = os.path.join(self.current_dir, folder_name)
+        if os.path.isdir(folder_path):
+            print(f"Removing existing folder: {folder_path}")
+            shutil.rmtree(folder_path)
+        os.mkdir(folder_path)
+        return folder_path
+
+    def fetch_emojis(self, guild_id):
+        response = requests.get(
+            f"https://discord.com/api/v8/guilds/{guild_id}/emojis",
+            headers={"authorization": self.token}
+        )
+        if response.status_code != 200:
+            print(f"Failed to fetch emojis: {response.status_code} {response.text}")
+            exit(1)
+        return response.json()
+
+    def download_emojis(self, emojis, folder_path):
+        for i, emoji in enumerate(emojis, start=1):
+            file_extension = "gif" if emoji["animated"] else "png"
+            file_name = f"{emoji['name']}.{file_extension}"
+            file_path = os.path.join(folder_path, file_name)
+
+            print(f"Downloading {emoji['name']} ({'animated' if emoji['animated'] else 'static'}) ({i}/{len(emojis)})")
+            response = requests.get(f"https://cdn.discordapp.com/emojis/{emoji['id']}.{file_extension}?v=1")
+            with open(file_path, "wb") as file:
+                file.write(response.content)
+
+        print("Finished downloading all emojis")
+
+    def run(self):
+        guilds = self.fetch_guilds()
+        guild_id, guild_name = self.choose_guild(guilds)
+        folder_name = self.restrict(guild_name)
+        folder_path = self.prepare_folder(folder_name)
+        emojis = self.fetch_emojis(guild_id)
+        self.download_emojis(emojis, folder_path)
 
 if __name__ == "__main__":
-    guilds = requests.get("https://discord.com/api/v8/users/@me/guilds", headers={"authorization": token}).json()
-    i = 0
-    msg = ""
-    server_ids = []
-    server_names = []
-    for guild in guilds:
-        i += 1
-        server_ids.append(guild["id"])
-        server_names.append(guild["name"])
-        msg += str(i) + " | " + guild["name"] + "\n"
-    print(msg)
-    a = input("Choose the guild : ")
-    if a.isdigit():
-        server_id = server_ids[int(a) + 1]
-        server_name = server_names[int(a) + 1]
-    else:
-        print("not a num")
-        exit(0)
-    server_folder = restrict(server_name)
-    if os.path.isdir("./" + server_folder):
-        print("removing the folder " + server_folder)
-        shutil.rmtree("./" + server_folder)
-    os.mkdir("./" + server_folder)
-    print("getting the emoji list of " + server_name)
-    emojis = requests.get("https://discord.com/api/v8/guilds/" + server_id + "/emojis", headers={"authorization": token}).json()
-    i = 0
-    for emoji in emojis:
-        i += 1
-        if emoji["animated"]:
-            with open("./" + server_folder + "/" + emoji["name"] + ".gif", "wb") as f:
-                print("downloading " + emoji["name"] + " (animated) (" + str(i) + "/" + str(len(emojis)) + ")")
-                f.write(requests.get("https://cdn.discordapp.com/emojis/" + emoji["id"] + ".gif?v=1").content)
-        else:
-            with open("./" + server_folder + "/" + emoji["name"] + ".png", "wb") as f:
-                print("downloading " + emoji["name"] + " (" + str(i) + "/" + str(len(emojis)) + ")")
-                f.write(requests.get("https://cdn.discordapp.com/emojis/" + emoji["id"] + ".png?v=1").content)
-    print("finished downloading all emojis")
+    downloader = DiscordEmojiDownloader()
+    downloader.run()
